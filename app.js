25ab7612b4b1692d367544b8c05e86f2e9c2705d
Allen <allenvs@mail.ru>
2020-08-08 18:59:19 +0300

    clean with comments

--- app.js
+++ app.js
@@ -2,18 +2,14 @@ let nums = [2, 7, 11, 15];
 let target = 9; 
 
 var twoSum = function(nums, target) {
-    let Map = {};
+    let Map = {}; // declare an object to build hash map for nums
     for (let i = 0; i < nums.length; i++) {
-        let complement = target - nums[i];
+        let complement = target - nums[i]; // calculate what value of the second number that matches the condition
+        // check if the second number is already a key in the current hash map
         if (complement.toString() in Map) {
-            let result = [Map[complement.toString()], i];
-            return result;
+            return [Map[complement.toString()], i]; // return value of matching key and current increment
         };
-        let Key = nums[i];  
-        Map[Key] = i;
+        Map[nums[i].toString()] = i; // add key nums[i] with value i
     };
-    
-    console.log(Object.keys(Map));
-    console.log(Map);
 };
-console.log(twoSum(nums, 13));
+console.log(twoSum(nums, target));
